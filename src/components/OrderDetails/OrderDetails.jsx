2f315ed29b228faa54552ceccf90e9239dd441a0
ZAPARY Anastasia <Anastasia.ZAPARY@partners.raiffeisen.ru>
2023-02-17 21:57:53 +0100

    use OrderDataContext in OrderDetails

--- src/components/OrderDetails/OrderDetails.jsx
+++ src/components/OrderDetails/OrderDetails.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import classnames from 'classnames'
-import check from '../../images/check-image.svg'
+import PropTypes from 'prop-types'
+import { useOrderData } from '../OrderDataProvider/OrderDataProvider'
 
+import check from '../../images/check-image.svg'
 import styles from './OrderDetails.module.css'
 
 
+OrderDetails.propTypes = {
+  orderNumber: PropTypes.number.isRequired
+}
+
 export default function OrderDetails(){
+  const {orderData} = useOrderData()
+  
   return (
     <div className={classnames(styles.wrapper, 'pt-4 pb-20', 'pl-15', 'pr-15')}>
       <h2 className='text text_type_digits-large pb-8'>
-        034536
+        {orderData?.number}
       </h2>
       <p className='text text_type_main-medium pb-15'>
         идентификатор заказа
@@ -19,7 +27,7 @@ export default function OrderDetails(){
         Ваш заказ начали готовить
       </p>
       <p className='text text_type_main-default  text_color_inactive'>
-          Дождитесь готовности на орбитальной станции
+        Дождитесь готовности на орбитальной станции
       </p>
     </div>
   )

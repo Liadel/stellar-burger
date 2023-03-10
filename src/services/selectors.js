a237b5d7ca66171704d73e52d08b33b0322584a7
ZAPARY Anastasia <Anastasia.ZAPARY@partners.raiffeisen.ru>
2023-03-10 10:50:32 +0100

    added selectors

---
+++ src/services/selectors.js
@@ -0,0 +1,5 @@
+export const selectUser = (state) => state.user
+export const selectIngredients = (state) => state.ingredients
+export const selectCurrentIngredient = (state) => state.currentIngredient
+export const selectConstructorItems = (state) => state.constructorItems
+export const selectOrder = (state) => state.order
